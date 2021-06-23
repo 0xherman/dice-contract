15a3570a9af14c4cb0d10239ed3a19a859f7b286
0xherman <0xhermandev@gmail.com>
2021-06-22 17:42:08 -0700

    Add min/max and paused

--- contracts/DiceRoll.sol
+++ contracts/DiceRoll.sol
@@ -8,25 +8,27 @@ contract DiceRoll is VRFConsumerBase, Ownable {
 	bytes32 private s_keyHash;
 	uint256 private s_fee;
 
-	uint256 public gameId;
-	uint256 public lastGameId;
-	uint256 private constant GAME_NOT_STARTED = 0;
-	uint256 private constant ROLL_IN_PROGRESS = 42;
+	uint256 private constant ROLL_IN_PROGRESS = 0;
+
+	bool public paused = true;
+	uint256 public minBid = 0;
+	uint256 private MAX_BID = 1 ether;
 
 	mapping(bytes32 => Game) public games;
+	mapping(address => bytes32[]) public gamesByAddress;
 
 	struct Game {
 		bytes32 id;
 		uint256 seed;
 		uint256 guess;
 		uint256 result;
-		uint256 amount;
+		uint256 bid;
 		address payable player;
 		bool paid;
 	}
 
 	event Received(address indexed sender, uint256 amount);
-	event DiceRolled(bytes32 indexed requestId);
+	event DiceRolled(bytes32 indexed requestId, uint256 bid);
 	event DiceLanded(bytes32 indexed requestId, uint256 indexed result);
 	event WinnerPaid(address indexed sender, bytes32 indexed requestId, uint256 amount);
 
@@ -39,14 +41,42 @@ contract DiceRoll is VRFConsumerBase, Ownable {
 		emit Received(msg.sender, msg.value);
 	}
 
+	function setPaused(bool _paused) external onlyOwner {
+		paused = _paused;
+	}
+
+	function setMinBid(uint256 _minBid) external onlyOwner {
+		require(_minBid > 0, "Minimum bid cannot be less than 0");
+		require(_minBid <= MAX_BID, "Minimum bid cannot be greater than maximum bid");
+		minBid = _minBid;
+	}
+
+	function setMaxBid(uint256 _maxBid) external onlyOwner {
+		require(_maxBid > 0, "Maximum bid cannot be less than 0");
+		require(_maxBid >= minBid, "Maximum bid cannot be less than minimum bid");
+		MAX_BID = _maxBid;
+	}
+
+	function maxBid() public view returns (uint256) {
+		uint256 _maxBid = address(this).balance.div(2);
+		if (MAX_BID < _maxBid) {
+			return MAX_BID;
+		}
+		return _maxBid;
+	}
+
 	function rollDice(uint256 guess, uint256 seed) external payable returns (bytes32 requestId) {
+		require(!paused, "Game is paused");
 		require(LINK.balanceOf(address(this)) >= s_fee, "Not enough LINK to pay fee");
 		require(address(this).balance >= msg.value, "Cannot bid more than held in pool");
+		require(msg.value <= maxBid(), "Bid value too high for current max bid or pool value");
+		require(msg.value >= minBid, "Bid value too low for current min bid");
 
 		requestId = requestRandomness(s_keyHash, s_fee, seed);
-		games[requestId] = Game(requestId, seed, guess, GAME_NOT_STARTED, msg.value, msg.sender, false);
+		games[requestId] = Game(requestId, seed, guess, ROLL_IN_PROGRESS, msg.value, msg.sender, false);
+		gamesByAddress[msg.sender].push(requestId);
 
-		emit DiceRolled(requestId);
+		emit DiceRolled(requestId, msg.value);
 	}
 
 	function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
@@ -54,18 +84,14 @@ contract DiceRoll is VRFConsumerBase, Ownable {
 		Game storage game = games[requestId];
 		game.result = d6Value;
 		emit DiceLanded(requestId, d6Value);
-
-		claim(requestId);
 	}
 
 	function checkResults(bytes32 requestId) external view returns (bool) {
-		require(games[requestId].result != GAME_NOT_STARTED, "Dice not rolled");
 		require(games[requestId].result != ROLL_IN_PROGRESS, "Roll in progress");
 		return games[requestId].result == games[requestId].guess;
 	}
 
 	function claim(bytes32 requestId) public virtual {
-		require(games[requestId].result != GAME_NOT_STARTED, "Dice not rolled");
 		require(games[requestId].result != ROLL_IN_PROGRESS, "Roll in progress");
 		require(games[requestId].player == msg.sender, "You cannot claim the rewards for this game");
 		require(games[requestId].guess == games[requestId].result, "You did not win this game");
@@ -79,7 +105,6 @@ contract DiceRoll is VRFConsumerBase, Ownable {
 	}
 
 	function withdrawClaim(bytes32 requestId) external virtual {
-		require(games[requestId].result != GAME_NOT_STARTED, "Dice not rolled");
 		require(games[requestId].result != ROLL_IN_PROGRESS, "Roll in progress");
 		require(games[requestId].player == msg.sender, "You cannot withdraw the bid for this game");
 		require(games[requestId].guess == games[requestId].result, "You did not win this game");
